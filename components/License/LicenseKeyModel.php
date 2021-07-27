6a253285021511063c3e0452a8380efcc1cb3086
Casey Milne <casey.milne@eatbuildplay.com>
2021-07-27 17:58:02 -0500

    Draft license key model.

---
+++ components/License/LicenseKeyModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SaberCommerce\Extension\Licensing;
+
+class LicenseKeyModel extends \SaberCommerce\Model {
+
+	function fields() {
+
+		$fields = [];
+
+		return $fields;
+
+	}
+
+}
